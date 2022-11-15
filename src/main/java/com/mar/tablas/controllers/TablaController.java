e10754611336b2a00cc1869dc4240e1b8515af72
marceloAar <marcelo_aar@hotmail.com>
2022-11-14 22:00:46 -0300

    Tabla multiplicar arreglada

--- src/main/java/com/mar/tablas/controllers/TablaController.java
+++ src/main/java/com/mar/tablas/controllers/TablaController.java
@@ -1,5 +1,8 @@
 package com.mar.tablas.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +21,35 @@ public class TablaController {
 	
 	
 	@PostMapping("/consultar")
-	public String consulta(@RequestParam(value = "numero") Integer numero, Model model){
-				
-		model.addAttribute("numero", numero);
+	public String consulta(@RequestParam(value = "numero") String numero, Model model){		
 		
+		//Integer mostrarDiv = 0;
 		
-		return "tablas";
-	}
-	
-	
+		if (numero.equals("0")) {
+			//mostrarDiv = 1;
+			model.addAttribute("validar", "Ingrese un número mayor a cero.");
+			return "index";
+		} 
+		  
+		if(numero.equals("")) {
+			//mostrarDiv = 1;
+			model.addAttribute("validar", "Debe ingresar un valor, campo no debe estar vacio.");		
+			return "index"; 
+		}
+		
+		Integer num = Integer.parseInt(numero);	
+		model.addAttribute("numero", num);
+		
+		List<Integer> listaN = new ArrayList<Integer>();
+		
+		for (int i = 1; i <= 10; i++) {
+			listaN.add(i);
+		}
+		
+		//mostrarDiv = false;				
+		model.addAttribute("multi", listaN);
+		return "tablas";	
+		
+	}		
 	
 }
